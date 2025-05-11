47bbec1ef60ca6ae759e31504f0ab85da2f75f88
Anshuman Padhi <liku1997@gmail.com>
2025-05-11 10:25:01 +0530

    clean up froentend and use hero ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import {createRoot} from 'react-dom/client'
-import './style.css'
 import App from './App'
+import './App.css';
+import {HeroUIProvider} from "@heroui/react";
 
 createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <HeroUIProvider>
         <App/>
-    </React.StrictMode>
+    </HeroUIProvider>
 )
